ae70a7160f1730dce28a25a64630119a221ebeb5
Camagny <robin2b2010@gmail.com>
2020-11-22 12:11:38 +0100

    implémentation interface utilisateur

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,147 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://www.aono.fr/">
+          <img src="./assets/AONO.jpg" alt="aono.fr">
+        </a>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="column">
+        <div class="title has-text-centered">Les équipes</div>
+        <div class="field is-grouped">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Nom de l'équipe">
+          </p>
+          <p class="control">
+            <button class="button is-info">
+              Ajouter
+            </button>
+          </p>
+        </div>
+        <table class="table is-fullwidth is-striped has-text-centered">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nom</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>nomequipe</td>
+              <td>
+                <div class="buttons are-small is-centered">
+                  <button class="button is-success">Modifier</button>
+                  <button class="button is-danger">Supprimer</button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>nomequipe2</td>
+              <td>
+                <div class="buttons are-small is-centered">
+                  <button class="button is-success">Modifier</button>
+                  <button class="button is-danger">Supprimer</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="column">
+        <div class="title has-text-centered">Les matchs</div>
+        
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <label class="label">Equipe Locale</label>
+              <div class="control is-expanded">
+                <div class="select">
+                  <select>
+                    <option>Select dropdown</option>
+                    <option>With options</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Equipe Visiteuse</label>
+              <div class="control is-expanded">
+                <div class="select">
+                  <select>
+                    <option>Select dropdown</option>
+                    <option>With options</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded has-text-centered">
+                <button class="button is-info">
+                  Ajouter
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <table class="table is-fullwidth is-striped has-text-centered">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Equipe locale</th>
+              <th>Equipe visiteuse</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>nomequipe</td>
+              <td>nomequipevisiteuse</td>
+              <td>
+                <div class="buttons are-small is-centered">
+                  <button class="button is-success">Modifier</button>
+                  <button class="button is-danger">Supprimer</button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>nomequipe2</td>
+              <td>nomequipevisiteuse2</td>
+              <td>
+                <div class="buttons are-small is-centered">
+                  <button class="button is-success">Modifier</button>
+                  <button class="button is-danger">Supprimer</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .is-vcentered{
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+  }
 </style>
